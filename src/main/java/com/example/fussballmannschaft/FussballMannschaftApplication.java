4430145e3229028e32775728b1bdd5ecde6def30
Z-Ramirez <96289837+Z-Ramirez@users.noreply.github.com>
2023-06-19 14:18:06 +0200

    test

---
+++ src/main/java/com/example/fussballmannschaft/FussballMannschaftApplication.java
@@ -0,0 +1,13 @@
+package com.example.fussballmannschaft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FussballMannschaftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FussballMannschaftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fussballmannschaft/FussballMannschaftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fussballmannschaft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FussballMannschaftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
